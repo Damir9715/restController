2771f3fbcf7420b592ca96e11d1a7173e7ca48a9
Damir <damir9715.com>
2018-12-20 20:15:26 +0600

    msg

---
+++ src/main/java/com/iitu/service/CustomerServiceImpl.java
@@ -0,0 +1,37 @@
+package com.iitu.service;
+
+import com.iitu.model.Customer;
+import com.iitu.repository.CustomerRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Override
+    public Customer getById(Long id) {
+        return customerRepository.getOne(id);
+    }
+
+    @Override
+    public void save(Customer customer) {
+        customerRepository.save(customer);
+    }
+
+    @Override
+    public void delete(Long id) {
+        customerRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Customer> getAll() {
+        return customerRepository.findAll();
+    }
+}
---
+++ src/main/resources/database/populateDB.sql
@@ -0,0 +1,2 @@
+INSERT INTO customers VALUES (1, 'John', 'Brzenk', 'USA', 10000.00);
+INSERT INTO customers VALUES (2, 'Devon', 'Larrat', 'Canada', 20000.00);
